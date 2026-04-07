d7f703a402c3f088b01f2dd5a49d129ec78cdcd0
jbrownwebsites <joebro84@yahoo.com>
2026-04-07 14:05:20 -0400

    Rename models remove plurals

--- test/app/Models/Story.php
+++ test/app/Models/Story.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
-class Stories extends Model
+class Story extends Model
 {
     /** @use HasFactory<\Database\Factories\StoryFactory> */
     use HasFactory;
