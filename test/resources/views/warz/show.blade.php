ec3c90f19c56b7ba51cd61994a8539c108368804
jbrownwebsites <joebro84@yahoo.com>
2026-04-19 11:12:21 -0400

    remove comments

--- test/resources/views/warz/show.blade.php
+++ test/resources/views/warz/show.blade.php
@@ -117,13 +117,6 @@
                                     <p class="font-normal text-[16px] truncate">
                                         {{ $user->name }}
                                     </p>
-                                    {{-- <p class="text-sm text-body truncate">
-                                        email@windster.com
-                                    </p> --}}
-
-                                    {{-- <div class="text-stone-600 p-2 bg-gray-200/90 text-xs h-full">
-                                        Voted for 
-                                    </div> --}}
                                 </div>
                                 <div class="inline-flex items-center glow-yellow font-bold text-3xl">
                                     {{ $user->score ?? 0 }}
@@ -133,135 +126,17 @@
                     @endforeach
                 </ul>
             </div>
-
-
-
-                        {{-- <div class="mb-8 text-white text-xl font-semibold items-center text-center">
-                            <div class="mb-0">
-                                <p class="glow-blue">
-                                    {{ $user->name }}
-                                </p>
-                            </div>
-
-                            <div class="">
-                                <p class="text-3xl glow-yellow">
-                                    0
-                                </p>
-                            </div>
-                        </div> --}}
-
-{{-- <div class="flow-root">
-    <ul role="list" class="divide-y divide-default">
-        <li class="py-4 sm:py-4">
-            <div class="flex items-center gap-2">
-                <div class="shrink-0">
-                    <img class="w-8 h-8 rounded-full" src="/docs/images/people/profile-picture-1.jpg" alt="Neil image">
-                </div>
-                <div class="flex-1 min-w-0 ms-2">
-                    <p class="font-medium text-heading truncate">
-                        Neil Sims
-                    </p>
-                    <p class="text-sm text-body truncate">
-                        email@windster.com
-                    </p>
-                </div>
-                <div class="inline-flex items-center font-medium text-heading">
-                    $320
-                </div>
-            </div>
-        </li>
-        <li class="py-4 sm:py-4">
-            <div class="flex items-center gap-2">
-                <div class="shrink-0">
-                    <img class="w-8 h-8 rounded-full" src="/docs/images/people/profile-picture-3.jpg" alt="Bonnie image">
-                </div>
-                <div class="flex-1 min-w-0 ms-2">
-                    <p class="font-medium text-heading truncate">
-                        Bonnie Green
-                    </p>
-                    <p class="text-sm text-body truncate">
-                        email@windster.com
-                    </p>
-                </div>
-                <div class="inline-flex items-center font-medium text-heading">
-                    $3467
-                </div>
-            </div>
-        </li>
-        <li class="py-4 sm:py-4">
-            <div class="flex items-center gap-2">
-                <div class="shrink-0">
-                    <img class="w-8 h-8 rounded-full" src="/docs/images/people/profile-picture-2.jpg" alt="Michael image">
-                </div>
-                <div class="flex-1 min-w-0 ms-2">
-                    <p class="font-medium text-heading truncate">
-                        Michael Gough
-                    </p>
-                    <p class="text-sm text-body truncate">
-                        email@windster.com
-                    </p>
-                </div>
-                <div class="inline-flex items-center font-medium text-heading">
-                    $67
-                </div>
-            </div>
-        </li>
-        <li class="py-4 sm:py-4">
-            <div class="flex items-center gap-2">
-                <div class="shrink-0">
-                    <img class="w-8 h-8 rounded-full" src="/docs/images/people/profile-picture-4.jpg" alt="Lana image">
-                </div>
-                <div class="flex-1 min-w-0 ms-2">
-                    <p class="font-medium text-heading truncate">
-                        Lana Byrd
-                    </p>
-                    <p class="text-sm text-body truncate">
-                        email@windster.com
-                    </p>
-                </div>
-                <div class="inline-flex items-center font-medium text-heading">
-                    $367
-                </div>
-            </div>
-        </li>
-        <li class="pt-4 pb-0">
-            <div class="flex items-center gap-2">
-                <div class="shrink-0">
-                    <img class="w-8 h-8 rounded-full" src="/docs/images/people/profile-picture-5.jpg" alt="Thomas image">
-                </div>
-                <div class="flex-1 min-w-0 ms-2">
-                    <p class="font-medium text-heading truncate">
-                        Thomas Lean
-                    </p>
-                    <p class="text-sm text-body truncate">
-                        email@windster.com
-                    </p>
-                </div>
-                <div class="inline-flex items-center font-medium text-heading">
-                    $2367
-                </div>
-            </div>
-        </li>
-    </ul>
-</div> --}}
-
-
-
-
         </div>
     </div>
-
-    {{-- <button data-modal-target="popup-modal" data-modal-toggle="popup-modal" class="text-white bg-brand box-border border border-transparent hover:bg-brand-strong focus:ring-4 focus:ring-brand-medium shadow-xs font-medium leading-5 rounded-base text-sm px-4 py-2.5 focus:outline-none" type="button">
-        Toggle modal
-    </button> --}}
 
     <div id="popup-modal" tabindex="-1" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
         <div class="relative p-4 w-full max-w-md max-h-full">
             <div class="relative bg-neutral-primary-soft border border-default dark:bg-stone-800/95 dark:border-stone-600 rounded-base shadow-sm p-4 md:p-6">
-                    <button type="button" class="cursor-pointer absolute top-3 inset-e-2.5 text-body bg-transparent hover:bg-neutral-tertiary hover:text-heading rounded-base text-sm w-9 h-9 ms-auto inline-flex justify-center items-center" data-modal-hide="popup-modal">
-                        <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18 17.94 6M18 18 6.06 6"/></svg>
-                        <span class="sr-only">Close modal</span>
-                    </button>
+                <button type="button" class="cursor-pointer absolute top-3 inset-e-2.5 text-body bg-transparent hover:bg-neutral-tertiary hover:text-heading rounded-base text-sm w-9 h-9 ms-auto inline-flex justify-center items-center" data-modal-hide="popup-modal">
+                    <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18 17.94 6M18 18 6.06 6"/></svg>
+                    <span class="sr-only">Close modal</span>
+                </button>
+
                 <div class="p-4 md:p-5 text-center">
                     <svg class="mx-auto mb-4 text-stone-400 w-12 h-12" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 13V8m0 8h.01M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"/></svg>
                     <h3 class="mb-6 text-stone-200 text-xl">Are you sure you want to vote for <span class="font-bold vote-name"></span>?</h3>
